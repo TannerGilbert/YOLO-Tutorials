c73b727df164b8441b7003370db4cf158b36f281
TannerGilbert <gilberttanner.work@gmail.com>
2020-05-02 00:10:53 +0200

    added output saving functionality to YOLO with OpenCV script

--- YOLO-Object-Detection-with-OpenCV/yolo.py
+++ YOLO-Object-Detection-with-OpenCV/yolo.py
@@ -75,6 +75,8 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--confidence', type=float, default=0.5, help='Minimum confidence for a box to be detected.')
     parser.add_argument('-t', '--threshold', type=float, default=0.3, help='Threshold for Non-Max Suppression')
     parser.add_argument('-u', '--use_gpu', default=False, action='store_true', help='Use GPU (OpenCV must be compiled for GPU). For more info checkout: https://www.pyimagesearch.com/2020/02/03/how-to-use-opencvs-dnn-module-with-nvidia-gpus-cuda-and-cudnn/')
+    parser.add_argument('-s', '--save', default=False, action='store_true', help='Whether or not the output should be saved')
+    parser.add_argument('-sh', '--show', default=True, action="store_false", help='Show output')
 
     input_group = parser.add_mutually_exclusive_group()
     input_group.add_argument('-i', '--image_path', type=str, default='', help='Path to the image file.')
@@ -96,6 +98,10 @@ if __name__ == '__main__':
         net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
         net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA)
 
+    if args.save:
+        print('Creating output directory if it doesn\'t already exist')
+        os.makedirs('output', exist_ok=True)
+
     # Get the ouput layer names
     layer_names = net.getLayerNames()
     layer_names = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
@@ -109,13 +115,24 @@ if __name__ == '__main__':
         image = draw_bounding_boxes(image, boxes, confidences, classIDs, idxs, colors)
 
         # show the output image
-        cv2.imshow('YOLO Object Detection', image)
-        cv2.waitKey(0)
+        if args.show:
+            cv2.imshow('YOLO Object Detection', image)
+            cv2.waitKey(0)
+        
+        if args.save:
+            cv2.imwrite(f'output/{args.image_path.split("/")[-1]}', image)
     else:
         if args.video_path != '':
             cap = cv2.VideoCapture(args.video_path)
         else:
             cap = cv2.VideoCapture(0)
+
+        if args.save:
+            width = int(cap.get(3))
+            height = int(cap.get(4))
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            name = args.video_path.split("/")[-1] if args.video_path else 'camera.avi'
+            out = cv2.VideoWriter(f'output/{name}', cv2.VideoWriter_fourcc('M','J','P','G'), fps, (width, height))
 
         while cap.isOpened():
             ret, image = cap.read()
@@ -124,7 +141,15 @@ if __name__ == '__main__':
 
             image = draw_bounding_boxes(image, boxes, confidences, classIDs, idxs, colors)
 
-            cv2.imshow('YOLO Object Detection', image)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-   
+            if args.show:
+                cv2.imshow('YOLO Object Detection', image)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            
+            if args.save:
+                out.write(image)
+    
+        cap.release()
+        if args.save:
+            out.release()
+    cv2.destroyAllWindows()
